e24ece4366e75d2e0369d6b7481018c37920f716
pnskrrt <111521025+pnskrrt@users.noreply.github.com>
2024-11-17 22:44:41 +0700

    Fixed Test Cases

--- test_zoo.py
+++ test_zoo.py
@@ -5,10 +5,30 @@ class TestZoo(unittest.TestCase):
     def setUp(self):
         self.zoo = Zoo()
 
+    def test_Infant_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), "Not Allow to Enter the Zoo")
+
     def test_child_ticket_price(self):
-        self.assertEqual(self.zoo.get_ticket_price(5), 50)
-       
-    # Add your additional test cases here.
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
+
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(12), 50)
+
+    def test_teen_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(13), 100)
+
+    def test_teen_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+
+    def test_adult_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+
+    def test_adult_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(60), 150)
+
+    def test_elder_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
+    
 
 if __name__ == '__main__':
     unittest.main()
